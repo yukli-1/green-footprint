a0f7e1e79f205ae5e14e4d6850c2a1e1db2a63c3
yukli-1 <2782438573@qq.com>
2025-12-29 16:43:35 +0800

    Add GitHub Actions auto-deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/green-footprint/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
